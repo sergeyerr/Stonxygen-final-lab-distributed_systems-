11ee4a8ed84f93e0fa0e0c3dc1413bbfdfc592d1
Sergei Voloboev <voloboeff2010@yandex.ru>
2020-12-11 00:01:34 +0500

    some update to old grpc; testing builds

--- Finance_API/Finance_API.py
+++ Finance_API/Finance_API.py
@@ -59,7 +59,12 @@ def serve():
         StocksLoaderServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    server.wait_for_termination()
+    #server.wait_for_termination()
+    try:
+        while True:
+            time.sleep(100500)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 
 if __name__ == '__main__':
